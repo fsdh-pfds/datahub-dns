efde3b05c305053ee6efacdba2e4ed91c60c12ad
John Bain <john.bain@gmail.com>
2025-03-25 11:48:24 -0400

    go again

--- dns/backend.tf
+++ dns/backend.tf
@@ -2,13 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"  # You can specify a version range or a fixed version
+      version = "~> 2.0"
     }
   }
 
   backend "azurerm" {
-    # Your backend configuration here
-    use_azuread_auth = true
+    use_azuread_auth      = true
     resource_group_name   = "${var.resource_group_name}"
     storage_account_name  = "${var.storage_account_name}"
     container_name        = "${var.container_name}"
@@ -18,4 +17,26 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  # Use Managed Identity with OIDC
+  use_msi              = true
+  client_id            = "${var.azure_client_id}"  # Will come from environment variable or secrets
+  tenant_id            = "${var.azure_tenant_id}"  # Will come from environment variable or secrets
+  subscription_id      = "${var.azure_subscription_id}"  # Will come from environment variable or secrets
+}
+
+# Variables to allow passing these values
+variable "azure_client_id" {
+  description = "The Client ID of the Managed Identity"
+  type        = string
+}
+
+variable "azure_tenant_id" {
+  description = "The Tenant ID of the Azure AD tenant"
+  type        = string
+}
+
+variable "azure_subscription_id" {
+  description = "The Subscription ID for the Azure account"
+  type        = string
 }
