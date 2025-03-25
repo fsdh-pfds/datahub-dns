251953ff5c6430c69957e1d23f030981e648b7a5
John Bain <john.bain@gmail.com>
2025-03-25 11:16:18 -0400

    nice

--- backend.tf
+++ backend.tf
@@ -1,7 +1,21 @@
 terraform {
-  backend "azurerm" {
-    use_azuread_auth = true
-    source  = "hashicorp/azurerm"
-    version = "~> 2.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"  # You can specify a version range or a fixed version
+    }
   }
+
+  backend "azurerm" {
+    # Your backend configuration here
+    use_azuread_auth = true
+    resource_group_name   = "${var.resource_group_name}"
+    storage_account_name  = "${var.storage_account_name}"
+    container_name        = "${var.container_name}"
+    key                   = "${var.backend_state_name}"
+  }
+}
+
+provider "azurerm" {
+  features {}
 }
