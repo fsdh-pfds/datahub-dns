cadbdfbfdd0d1871d7c10b9819a776910e9efe14
John Bain <john.bain@gmail.com>
2025-03-27 01:06:43 +0000

    test contraint

--- main.tf
+++ main.tf
@@ -4,17 +4,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.24"
+      version = ">= 4.24, < 4.25"
     }
   }
 
   backend "azurerm" {
     //comment out 'use_azuread_auth' line to run locally
-    use_azuread_auth      = true
-    resource_group_name   = var.resource_group_name
-    storage_account_name  = var.storage_account_name
-    container_name        = var.container_name
-    key                   = var.backend_state_name
+    use_azuread_auth     = true
+    resource_group_name  = var.resource_group_name
+    storage_account_name = var.storage_account_name
+    container_name       = var.container_name
+    key                  = var.backend_state_name
   }
 }
 
