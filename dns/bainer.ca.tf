65f5c1390a7d76b51f6bf7d77a0d4ad6910bc1a0
John Bain <john.bain@gmail.com>
2025-03-27 00:10:07 +0000

    afsdaf

--- dns/bainer.ca.tf
+++ dns/bainer.ca.tf
@@ -1,11 +1,16 @@
+//bainer.ca.tf
 resource "azurerm_dns_zone" "bainer_ca_zone" {
   name                = "bainer.ca"
   resource_group_name = var.resource_group_name
 }
 
+locals {
+  zone_name = azurerm_dns_zone.bainer_ca_zone.name
+}
+
 resource "azurerm_dns_a_record" "bainer_ca_www" {
   name                = "www"
-  zone_name           = azurerm_dns_zone.bainer_ca_zone.name
+  zone_name           = local.zone_name
   resource_group_name = var.resource_group_name
   ttl                 = 3600
   records             = ["1.2.3.4"]
@@ -13,7 +18,7 @@ resource "azurerm_dns_a_record" "bainer_ca_www" {
 
 resource "azurerm_dns_cname_record" "bainer_ca_blog" {
   name                = "blog"
-  zone_name           = azurerm_dns_zone.bainer_ca_zone.name
+  zone_name           = local.zone_name
   resource_group_name = var.resource_group_name
   ttl                 = 3600
   record              = "bainer.github.io"
