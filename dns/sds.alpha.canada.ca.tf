0d1009376c049d4b9150b56522779c69af0b7918
John Bain <john.bain@gmail.com>
2025-06-25 15:13:32 -0400

    ASDAS

--- dns/sds.alpha.canada.ca.tf
+++ dns/sds.alpha.canada.ca.tf
@@ -5,7 +5,7 @@ resource "azurerm_dns_zone" "sds_alpha_canada_ca_zone" {
 
 
 #tags will be applied later
-  lifecycle {
-    ignore_changes = [tags] 
-  }
+#  lifecycle {
+#    ignore_changes = [tags] 
+#  }
 }
