06d053c116380484437170afb2248055f934785b
John Bain <john.bain@gmail.com>
2025-06-25 15:29:50 -0400

    remove changes

--- dns/sds.alpha.canada.ca.tf
+++ dns/sds.alpha.canada.ca.tf
@@ -2,10 +2,4 @@
 resource "azurerm_dns_zone" "sds_alpha_canada_ca_zone" {
   name                = "sds.alpha.canada.ca"
   resource_group_name = var.resource_group_name
-
-
-#tags will be applied later
-#  lifecycle {
-#    ignore_changes = [tags] 
-#  }
 }
