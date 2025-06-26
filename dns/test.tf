4fd232c0b7131f2e7aa7f9dcfda5fd9ab821df40
John Bain <john.bain@gmail.com>
2025-06-26 17:56:53 -0400

    should pass

--- dns/test.tf
+++ dns/test.tf
@@ -2,4 +2,7 @@
 resource "azurerm_dns_zone" "test_alpha_canada_ca_zone" {
   name                = "test.alpha.canada.ca"
   resource_group_name = var.resource_group_name
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
