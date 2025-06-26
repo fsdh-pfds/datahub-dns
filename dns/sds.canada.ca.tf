7364c4bea7255c5bf7f9a3d90f62976284ab639f
John Bain <john.bain@gmail.com>
2025-06-26 17:02:00 -0400

    initial commit

---
+++ dns/sds.canada.ca.tf
@@ -0,0 +1,38 @@
+# sds.canada.ca.tf
+
+resource "azurerm_dns_zone" "sds_canada_ca_zone" {
+  name                = "sds.canada.ca"
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_dns_a_record" "sds_canada_ca_a_record" {
+  name                = "@"
+  zone_name           = azurerm_dns_zone.sds_canada_ca_zone.name
+  resource_group_name = var.resource_group_name
+  ttl                 = 3600
+  records = [
+    "185.199.108.153",
+    "185.199.109.153",
+    "185.199.110.153",
+    "185.199.111.153"
+  ]
+}
+
+resource "azurerm_dns_txt_record" "sds_canada_ca_txt_github_pages_challenge_record" {
+  name                = "_github-pages-challenge-fsdh-pfds"
+  zone_name           = azurerm_dns_zone.sds_canada_ca_zone.name
+  resource_group_name = var.resource_group_name
+  ttl                 = 3600
+
+  record {
+    value = "5910da3f935f005ba8fa51eaca6495"
+  }
+}
+
+resource "azurerm_dns_cname_record" "sds_canada_ca_cname_www" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.sds_canada_ca_zone.name
+  resource_group_name = var.resource_group_name
+  ttl                 = 3600
+  record              = "sds.canada.ca"
+}
