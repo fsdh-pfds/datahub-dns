4201c3aa0ae8701a0b02441e47e213b301d180ba
John Bain <John.Bain@phac-aspc.gc.ca>
2025-06-27 00:01:01 +0000

    updated

--- dns/test.tf
+++
@@ -1,8 +0,0 @@
-# test.tf
-resource "azurerm_dns_zone" "test_alpha_canada_ca_zone" {
-  name                = "test.alpha.canada.ca"
-  resource_group_name = var.resource_group_name
-  lifecycle {
-    ignore_changes = [tags]
-  }
-}
