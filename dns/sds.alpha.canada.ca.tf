2b1034cd9b40676baeda2d9ff519221b1fe37345
John Bain <john.bain@gmail.com>
2025-06-27 08:18:43 -0400

    fixed

--- dns/sds.alpha.canada.ca.tf
+++ dns/sds.alpha.canada.ca.tf
@@ -2,4 +2,8 @@
 resource "azurerm_dns_zone" "sds_alpha_canada_ca_zone" {
   name                = "sds.alpha.canada.ca"
   resource_group_name = var.resource_group_name
+
+  lifecycle {
+    ignore_changes = ["tags"]
+  }
 }
