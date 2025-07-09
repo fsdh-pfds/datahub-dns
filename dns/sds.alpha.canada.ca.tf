f6bb3b06b08a0862a779e46e8efecec1b89a0072
John Bain <john.bain@gmail.com>
2025-07-09 14:34:40 -0400

    initial

--- dns/sds.alpha.canada.ca.tf
+++ dns/sds.alpha.canada.ca.tf
@@ -3,3 +3,11 @@ resource "azurerm_dns_zone" "sds_alpha_canada_ca_zone" {
   name                = "sds.alpha.canada.ca"
   resource_group_name = var.resource_group_name
 }
+
+resource "azurerm_dns_cname_record" "redirection_sds_alpha_canada_ca_cname" {
+  name                = "redirection"
+  zone_name           = "sds.alpha.canada.ca"
+  resource_group_name = var.resource_group_name
+  ttl                 = 300
+  record              = "fsdh-pfds.github.io"
+}
