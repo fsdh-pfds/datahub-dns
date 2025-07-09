f6c72c5cd184ee400519648680912e5e73fdac8e
John Bain <john.bain@gmail.com>
2025-07-09 14:44:36 -0400

    Sds redirector (#46)
    created static site for redirector with tracking

--- dns/sds.alpha.canada.ca.tf
+++ dns/sds.alpha.canada.ca.tf
@@ -2,4 +2,18 @@
 resource "azurerm_dns_zone" "sds_alpha_canada_ca_zone" {
   name                = "sds.alpha.canada.ca"
   resource_group_name = var.resource_group_name
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_dns_cname_record" "redirection_sds_alpha_canada_ca_cname" {
+  name                = "redirection"
+  zone_name           = "sds.alpha.canada.ca"
+  resource_group_name = var.resource_group_name
+  ttl                 = 300
+  record              = "fsdh-pfds.github.io"
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
