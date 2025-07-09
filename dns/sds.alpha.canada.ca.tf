9a9877883274f447cd42f3af46892b8b3b3a02e7
John Bain <john.bain@gmail.com>
2025-07-09 14:35:46 -0400

    include ignore tags

--- dns/sds.alpha.canada.ca.tf
+++ dns/sds.alpha.canada.ca.tf
@@ -2,6 +2,9 @@
 resource "azurerm_dns_zone" "sds_alpha_canada_ca_zone" {
   name                = "sds.alpha.canada.ca"
   resource_group_name = var.resource_group_name
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
 
 resource "azurerm_dns_cname_record" "redirection_sds_alpha_canada_ca_cname" {
@@ -10,4 +13,7 @@ resource "azurerm_dns_cname_record" "redirection_sds_alpha_canada_ca_cname" {
   resource_group_name = var.resource_group_name
   ttl                 = 300
   record              = "fsdh-pfds.github.io"
+  lifecycle {
+    ignore_changes = [tags]
+  }
 }
